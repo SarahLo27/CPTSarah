d30a4c4a5d4b7b4f7bd215dad1b164c36d19fbe4
SarahLo27 <sarah.lo27@ycdsbk12.ca>
2025-01-15 09:29:28 -0500

    Main Menu Updates & Themes
    - able to return back to main menu after finished playing and review high scores
    - started working on the themes

--- FinalCPTSarah.java
+++ FinalCPTSarah.java
@@ -1,4 +1,5 @@
 import arc.*;
+import java.awt.*;
 import java.awt.image.*;
 
 public class FinalCPTSarah{
@@ -13,25 +14,22 @@ public class FinalCPTSarah{
 		console.sleep(3000);
 		console.closeWindow();
 		
-		// Create Default Variables
-		boolean blnPlay = false;
-		boolean blnChoice = true;
-		char chrChoice = '1';
-		
 		// Set Default Player Names
 		String strPlayerInfo[][];
 		strPlayerInfo = new String[2][2]; 
 		strPlayerInfo[0][0] = "";
 		
-		// Set Default Turns
-		int intPlayerTurn = 0;
-		String strPlayAgain;
-		
-		// Set Default Choice & Gameplay Variables
+		// Create and Set Default Choice & Gameplay Variables
+		boolean blnPlay = false;
+		boolean blnChoice = true;
+		char chrChoice = '1';
 		int intColumnChoice = 0;
 		boolean blnPlay2;
 		String strPlayerPiece;
 		boolean blnGameplay = true;
+		int intPlayerTurn = 0;
+		String strPlayAgain;
+		boolean blnEnd = false;
 		
 		// Set Default Wins
 		int intPlayer1Wins;
@@ -45,222 +43,224 @@ public class FinalCPTSarah{
 		// Main Menu
 		Console con = new Console();
 		
-		while(blnPlay == false){
-			// Show Menu Screen
-			while(blnChoice == true){
-				con.clear();
-				mainMenu(con);
-				blnChoice = false;
-			// Menu Choice
-			}while(blnChoice == false) {
-				// (P)lay
-				if(chrChoice == 'P' || chrChoice == 'p'){
+		while(blnEnd == false){		
+			while(blnPlay == false){
+				// Show Menu Screen
+				while(blnChoice == true){
 					con.clear();
-					con.println("PLAY");
-					blnPlay = true;
-					
-					// Ask for Name
-					con.print("Player 1, please enter your name: ");
-					strPlayerInfo[0][0] = con.readLine();
-					con.print("Player 2, please enter your name: ");
-					strPlayerInfo[0][1] = con.readLine();
+					mainMenu(con);
+					blnChoice = false;
+				// Menu Choice
+				}while(blnChoice == false) {
+					// (P)lay
+					if(chrChoice == 'P' || chrChoice == 'p'){
+						con.clear();
+						con.println("PLAY");
+						blnPlay = true;
+						
+						// Ask for Name
+						con.print("Player 1, please enter your name: ");
+						strPlayerInfo[0][0] = con.readLine();
+						con.print("Player 2, please enter your name: ");
+						strPlayerInfo[0][1] = con.readLine();
+						con.println(" ");
+						
+						break;
+					// (V)iew Highscores
+					}else if(chrChoice == 'V' || chrChoice == 'v'){
+						con.clear();
+						con.println("VIEW HIGHSCORES");
+						viewHighScores(con);
+						while(blnChoice == false){
+							if (chrChoice == 'M' || chrChoice == 'm'){
+								blnChoice = true;
+							}else{
+								chrChoice = 'V';
+								chrChoice = con.getChar();
+							}
+						}
+					// (T)heme
+					}else if(chrChoice == 'T' || chrChoice == 't'){
+						con.clear();
+						con.println("THEME");
+						// **ADD THEME COMMAND LATER 
+						while(blnChoice == false){
+							if (chrChoice == 'M' || chrChoice == 'm'){
+								blnChoice = true;
+							}else{
+								chrChoice = 'T';
+								chrChoice = con.getChar();
+							}
+						}
+					// (H)elp Menu
+					}else if(chrChoice == 'H' || chrChoice == 'h'){
+						con.clear();
+						con.println("HELP MENU");
+						helpMenu(con);
+						while(blnChoice == false){
+							if (chrChoice == 'M' || chrChoice == 'm'){
+								blnChoice = true;
+							}else{
+								chrChoice = 'H';
+								chrChoice = con.getChar();
+							}
+						}
+					// (Q)uit
+					}else if(chrChoice == 'Q' || chrChoice == 'q'){
+						con.clear();
+						con.println("QUIT");
+						con.sleep(1000);
+						con.closeConsole();
+					// Choose Menu Option
+					}else{
+						chrChoice = con.getChar();
+						con.println(chrChoice);
+					}
+				}
+			}
+			
+			while(blnGameplay == true){
+				// Set Default Board
+				String strBoard[][];
+				strBoard = new String[6][7]; 
+				int intRow = 0;
+				int intColumn = 0;
+				
+				for(intRow = 0; intRow < 6; intRow++){
+					for(intColumn = 0; intColumn < 7; intColumn++){
+						strBoard[intRow][intColumn] = "| ";
+						con.print(strBoard[intRow][intColumn]);
+					}
+					con.println("|");
+				}
+				con.println("---------------");
+				con.println(" 1 2 3 4 5 6 7");
+				
+				boolean blnInteraction;
+				
+				while(blnPlay2 == true){
+					// Display Score @ Top
+					con.println(" ");
+					con.println("SCORE--------------------------------------------------------------------------");
+					con.println(strPlayerInfo[0][0] + ": " + intPlayer1Wins);
+					con.println(strPlayerInfo[0][1] + ": " + intPlayer2Wins);
 					con.println(" ");
 					
-					break;
-				// (V)iew Highscores
-				}else if(chrChoice == 'V' || chrChoice == 'v'){
-					con.clear();
-					con.println("VIEW HIGHSCORES");
-					viewHighScores(con);
-					while(blnChoice == false){
-						if (chrChoice == 'M' || chrChoice == 'm'){
-							blnChoice = true;
+					// Determine whether 1 or 2 goes - 1 go if ODD
+					if(intPlayerTurn % 2 != 1){ 
+						strPlayerPiece = "|O";
+						blnInteraction = true;
+						con.println(strPlayerInfo[0][intPlayerTurn]+"'s (1) turn ---------------------------------------");
+						while(blnInteraction == true){
+							con.print("Please choose a column (type the number): ");
+							intColumnChoice = con.readInt();
+							// Validate Column Choice
+							if(intColumnChoice < 1 || intColumnChoice > 7){
+								con.println("Invalid column. Please choose a column between 1 & 7.");
+							}else if(!strBoard[0][intColumnChoice - 1].equals("| ")) {
+								con.println("Column is full. Please choose another column.");
+							}else{
+								// Valid Column Choice
+								blnInteraction = false;
+								intColumnChoice -= 1;
+							}
+						}
+						con.println("");
+						// Add Piece to Board
+						for(intRow = 5; intRow >= 0; intRow--){
+							if(strBoard[intRow][intColumnChoice].equals("| ")){ 
+								strBoard[intRow][intColumnChoice] = strPlayerPiece;
+								break;
+							}
+						}
+						// Redraw Entire Board
+						for(intRow = 0; intRow < 6; intRow++){
+							for(intColumn = 0; intColumn < 7; intColumn++){
+								con.print(strBoard[intRow][intColumn]);
+							}
+							con.println("|"); 
+						}
+						con.println("---------------");
+						con.println(" 1 2 3 4 5 6 7");
+						
+						// Check Connect 4
+						if(checkConnect4(strBoard, strPlayerPiece)){
+							con.println(strPlayerInfo[0][0] + " wins!");
+							intPlayer1Wins += 1;
+							blnPlay2 = false;
 						}else{
-							chrChoice = 'V';
-							chrChoice = con.getChar();
+							// Update Player Turn
+							intPlayerTurn += 1;
 						}
-					}
-				// (T)heme
-				}else if(chrChoice == 'T' || chrChoice == 't'){
-					con.clear();
-					con.println("THEME");
-					// **ADD THEME COMMAND LATER 
-					while(blnChoice == false){
-						if (chrChoice == 'M' || chrChoice == 'm'){
-							blnChoice = true;
-						}else{
-							chrChoice = 'T';
-							chrChoice = con.getChar();
-						}
-					}
-				// (H)elp Menu
-				}else if(chrChoice == 'H' || chrChoice == 'h'){
-					con.clear();
-					con.println("HELP MENU");
-					helpMenu(con);
-					while(blnChoice == false){
-						if (chrChoice == 'M' || chrChoice == 'm'){
-							blnChoice = true;
-						}else{
-							chrChoice = 'H';
-							chrChoice = con.getChar();
-						}
-					}
-				// (Q)uit
-				}else if(chrChoice == 'Q' || chrChoice == 'q'){
-					con.clear();
-					con.println("QUIT");
-					con.sleep(1000);
-					con.closeConsole();
-				// Choose Menu Option
-				}else{
-					chrChoice = con.getChar();
-					con.println(chrChoice);
-				}
-			}
-		}
-		
-		while(blnGameplay == true){
-			// Set Default Board
-			String strBoard[][];
-			strBoard = new String[6][7]; 
-			int intRow = 0;
-			int intColumn = 0;
-			
-			for(intRow = 0; intRow < 6; intRow++){
-				for(intColumn = 0; intColumn < 7; intColumn++){
-					strBoard[intRow][intColumn] = "| ";
-					con.print(strBoard[intRow][intColumn]);
-				}
-				con.println("|");
-			}
-			con.println("---------------");
-			con.println(" 1 2 3 4 5 6 7");
-			
-			boolean blnInteraction;
-			
-			while(blnPlay2 == true){
-				// Display Score @ Top
-				con.println(" ");
-				con.println("SCORE--------------------------------------------------------------------------");
-				con.println(strPlayerInfo[0][0] + ": " + intPlayer1Wins);
-				con.println(strPlayerInfo[0][1] + ": " + intPlayer2Wins);
-				con.println(" ");
-				
-				// Determine whether 1 or 2 goes - 1 go if ODD
-				if(intPlayerTurn % 2 != 1){ 
-					strPlayerPiece = "|O";
-					blnInteraction = true;
-					con.println(strPlayerInfo[0][intPlayerTurn]+"'s (1) turn ---------------------------------------");
-					while(blnInteraction == true){
-						con.print("Please choose a column (type the number): ");
-						intColumnChoice = con.readInt();
-						// Validate Column Choice
-						if(intColumnChoice < 1 || intColumnChoice > 7){
-							con.println("Invalid column. Please choose a column between 1 & 7.");
-						}else if(!strBoard[0][intColumnChoice - 1].equals("| ")) {
-							con.println("Column is full. Please choose another column.");
-						}else{
-							// Valid Column Choice
-							blnInteraction = false;
-							intColumnChoice -= 1;
-						}
-					}
-					con.println("");
-					// Add Piece to Board
-					for(intRow = 5; intRow >= 0; intRow--){
-						if(strBoard[intRow][intColumnChoice].equals("| ")){ 
-							strBoard[intRow][intColumnChoice] = strPlayerPiece;
-							break;
-						}
-					}
-					// Redraw Entire Board
-					for(intRow = 0; intRow < 6; intRow++){
-						for(intColumn = 0; intColumn < 7; intColumn++){
-							con.print(strBoard[intRow][intColumn]);
-						}
-						con.println("|"); 
-					}
-					con.println("---------------");
-					con.println(" 1 2 3 4 5 6 7");
-					
-					// Check Connect 4
-					if(checkConnect4(strBoard, strPlayerPiece)){
-						con.println(strPlayerInfo[0][0] + " wins!");
-						intPlayer1Wins += 1;
-						blnPlay2 = false;
 					}else{
-						// Update Player Turn
-						intPlayerTurn += 1;
-					}
-				}else{
-					strPlayerPiece = "|X";
-					blnInteraction = true;
-					con.println(strPlayerInfo[0][1]+"'s (2) turn");
-					while(blnInteraction == true){
-						con.print("Please choose a column (type the number): ");
-						intColumnChoice = con.readInt();
-						// Validate Column Choice
-						if(intColumnChoice < 1 || intColumnChoice > 7){
-							con.println("Invalid column. Please choose a column between 1 & 7.");
-						}else if(!strBoard[0][intColumnChoice - 1].equals("| ")) {
-							con.println("Column is full. Please choose another column.");
+						strPlayerPiece = "|X";
+						blnInteraction = true;
+						con.println(strPlayerInfo[0][1]+"'s (2) turn");
+						while(blnInteraction == true){
+							con.print("Please choose a column (type the number): ");
+							intColumnChoice = con.readInt();
+							// Validate Column Choice
+							if(intColumnChoice < 1 || intColumnChoice > 7){
+								con.println("Invalid column. Please choose a column between 1 & 7.");
+							}else if(!strBoard[0][intColumnChoice - 1].equals("| ")) {
+								con.println("Column is full. Please choose another column.");
+							}else{
+								// Valid Column Choice
+								blnInteraction = false;
+								intColumnChoice -= 1;
+							}
+						}
+						con.println("");
+						// Add Piece to Board
+						for(intRow = 5; intRow >= 0; intRow--){
+							if(strBoard[intRow][intColumnChoice].equals("| ")){ 
+								strBoard[intRow][intColumnChoice] = strPlayerPiece;
+								break;
+							}
+						}
+						// Redraw Entire Board
+						for(intRow = 0; intRow < 6; intRow++){
+							for(intColumn = 0; intColumn < 7; intColumn++){
+								con.print(strBoard[intRow][intColumn]);
+							}
+							con.println("|"); 
+						}
+						con.println("---------------");
+						con.println(" 1 2 3 4 5 6 7");
+						
+						// Check Connect 4
+						if(checkConnect4(strBoard, strPlayerPiece)){
+							con.println(strPlayerInfo[0][1] + " wins!");
+							intPlayer2Wins += 1;
+							blnPlay2 = false;
 						}else{
-							// Valid Column Choice
-							blnInteraction = false;
-							intColumnChoice -= 1;
+							// Update Player Turn
+							intPlayerTurn -= 1;
 						}
-					}
-					con.println("");
-					// Add Piece to Board
-					for(intRow = 5; intRow >= 0; intRow--){
-						if(strBoard[intRow][intColumnChoice].equals("| ")){ 
-							strBoard[intRow][intColumnChoice] = strPlayerPiece;
-							break;
-						}
-					}
-					// Redraw Entire Board
-					for(intRow = 0; intRow < 6; intRow++){
-						for(intColumn = 0; intColumn < 7; intColumn++){
-							con.print(strBoard[intRow][intColumn]);
-						}
-						con.println("|"); 
-					}
-					con.println("---------------");
-					con.println(" 1 2 3 4 5 6 7");
-					
-					// Check Connect 4
-					if(checkConnect4(strBoard, strPlayerPiece)){
-						con.println(strPlayerInfo[0][1] + " wins!");
-						intPlayer2Wins += 1;
-						blnPlay2 = false;
-					}else{
-						// Update Player Turn
-						intPlayerTurn -= 1;
-					}
-				}			
-			}		
-			
-			// Display Wins
-			con.println(" ");
-			con.println(strPlayerInfo[0][0] + " has " + intPlayer1Wins + " wins.");
-			con.println(strPlayerInfo[0][1] + " has " + intPlayer2Wins + " wins.");
-			
-			con.println("Do you want to play again?");
-			strPlayAgain = con.readLine();
-			
-			if(strPlayAgain.equalsIgnoreCase("yes")){
-				blnPlay2 = true;
-			}else{
-				blnGameplay = false;
+					}			
+				}		
 				
-				// Add to Highscores **CHECK FILE LATER
-				TextOutputFile highscore = new TextOutputFile("highscores.txt", true);
-				highscore.println(strPlayerInfo[0][0]);
-				highscore.println(intPlayer1Wins);
-				highscore.println(strPlayerInfo[0][1]);
-				highscore.println(intPlayer2Wins);
-				highscore.close();
+				// Display Wins
+				con.println(" ");
+				con.println(strPlayerInfo[0][0] + " has " + intPlayer1Wins + " wins.");
+				con.println(strPlayerInfo[0][1] + " has " + intPlayer2Wins + " wins.");
+				con.println("Do you want to play again?");
+				strPlayAgain = con.readLine();
+				if(strPlayAgain.equalsIgnoreCase("yes")){
+					blnPlay2 = true;
+				}else{
+					blnGameplay = false;
+					// Add to Highscores
+					TextOutputFile highscore = new TextOutputFile("highscores.txt", true);
+					highscore.println(strPlayerInfo[0][0]);
+					highscore.println(intPlayer1Wins);
+					highscore.println(strPlayerInfo[0][1]);
+					highscore.println(intPlayer2Wins);
+					highscore.close();
+					blnChoice = true;
+					blnPlay = false;
+					chrChoice = '1';
+				}
 			}
 		}
 	}		
@@ -293,7 +293,7 @@ public class FinalCPTSarah{
 	
 	// View High Scores
 	public static void viewHighScores(Console con){
-		con.println("HIGH SCORES--------------------------------------------------------------------");
+		con.println("HIGH SCORES (TOP 10)-----------------------------------------------------------");
 		
 		// Open Highscores File
 		TextInputFile highscore = new TextInputFile("highscores.txt");
